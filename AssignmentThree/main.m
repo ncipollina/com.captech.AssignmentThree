a6af588162db57edab7f9cc123c05344f8cdd4b2
Nick Cipollina <ncipollina@gmail.com>
2012-04-09 14:47:03 -0400

    Initial Commit

---
+++ AssignmentThree/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AssignmentThree
+//
+//  Created by Nicholas Cipollina on 4/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "A3AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([A3AppDelegate class]));
+    }
+}
